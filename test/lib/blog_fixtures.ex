88bdc88e306f5c984e5425a3f5093ce8878ddf58
jordan <sudo.sendmail.jordan@gmail.com>
2025-12-04 17:38:18 -0800

    sync

---
+++ test/lib/blog_fixtures.ex
@@ -0,0 +1,21 @@
+defmodule Tripwire.BlogFixtures do
+  @moduledoc """
+  This module defines test helpers for creating
+  entities via the `Tripwire.Blog` context.
+  """
+
+  @doc """
+  Generate a post.
+  """
+  def post_fixture(attrs \\ %{}) do
+    {:ok, post} =
+      attrs
+      |> Enum.into(%{
+        body: "some body",
+        title: "some title"
+      })
+      |> Tripwire.Blog.create_post()
+
+    post
+  end
+end
--- web/tripwire_web/views/dashboard_html.ex
+++ web/tripwire_web/views/dashboard_html.ex
@@ -1,4 +1,4 @@
 defmodule TripwireWeb.DashboardHTML do
   use TripwireWeb, :html
-  embed_templates "../pages/dashboard.html"
+  embed_templates "dashboard.html"
 end
