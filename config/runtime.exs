e5a697cdd44c26929dc6f12a4b118fb36a146c2e
jordan <sudo.sendmail.jordan@gmail.com>
2025-12-04 21:55:59 -0800

    Update runtime.exs

--- config/runtime.exs
+++ config/runtime.exs
@@ -34,12 +34,16 @@ if config_env() == :prod do
   maybe_ipv6 = if System.get_env("ECTO_IPV6") in ~w(true 1), do: [:inet6], else: []
 
   config :tripwire, Tripwire.Repo,
-    # ssl: true,
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
     # For machines with several cores, consider starting multiple pools of `pool_size`
     # pool_count: 4,
     socket_options: maybe_ipv6
+    ssl: true,
+    ssl_opts: [
+      verify: :verify_none,
+      cacerts: :public_key.cacerts_get()
+    ]
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
   # A default value is used in config/dev.exs and config/test.exs but you
