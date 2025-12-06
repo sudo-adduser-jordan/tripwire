a665bb12628fafa5310e9711cba7e723b3469f4c
jordan <sudo.sendmail.jordan@gmail.com>
2025-12-05 17:24:07 -0800

    configure database / update home css

--- config/config.exs
+++ config/config.exs
@@ -1,17 +1,9 @@
-# This file is responsible for configuring your application
-# and its dependencies with the aid of the Config module.
-#
-# This configuration file is loaded before any dependency and
-# is restricted to this project.
-
-# General application configuration
 import Config
 
 config :tripwire,
   ecto_repos: [Tripwire.Repo],
   generators: [timestamp_type: :utc_datetime]
 
-# Configure the endpoint
 config :tripwire, TripwireWeb.Endpoint, server: true,
   url: [host: "localhost"],
   adapter: Bandit.PhoenixAdapter,
@@ -22,16 +14,7 @@ config :tripwire, TripwireWeb.Endpoint, server: true,
   pubsub_server: Tripwire.PubSub,
   live_view: [signing_salt: "2qoLjCJq"]
 
-# Configure the mailer
-#
-# By default it uses the "Local" adapter which stores the emails
-# locally. You can see the emails in your browser, at "/dev/mailbox".
-#
-# For production it's recommended to configure a different adapter
-# at the `config/runtime.exs`.
-config :tripwire, Tripwire.Mailer, adapter: Swoosh.Adapters.Local
 
-# Configure esbuild (the version is required)
 config :esbuild,
   version: "0.25.4",
   tripwire: [
@@ -41,7 +24,6 @@ config :esbuild,
     env: %{"NODE_PATH" => [Path.expand("../deps", __DIR__), Mix.Project.build_path()]}
   ]
 
-# Configure tailwind (the version is required)
 config :tailwind,
   version: "4.1.12",
   tripwire: [
@@ -52,14 +34,11 @@ config :tailwind,
     cd: Path.expand("..", __DIR__)
   ]
 
-# Configure Elixir's Logger
 config :logger, :default_formatter,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
-# Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
+config :tripwire, Tripwire.Mailer, adapter: Swoosh.Adapters.Local
 
-# Import environment specific config. This must remain at the bottom
-# of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
