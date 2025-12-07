6b3e7063b11d07e08f12630ab2579f25229754ce
jordan <sudo.sendmail.jordan@gmail.com>
2025-12-06 16:23:24 -0800

    add background / fix toggle

--- assets/js/app.js
+++ assets/js/app.js
@@ -23,10 +23,13 @@ const options = {
 var grid = GridStack.init(options);
 
 import * as echarts from 'echarts';
-import $ from "jquery";
+// import $ from "jquery";
 
-var chartDom = document.getElementById('map');
-var myChart1 = echarts.init(chartDom);
+// var chartDom = document.getElementById('map');
+
+document.addEventListener('DOMContentLoaded', () => {
+    var myChart1 = echarts.init(chartDom);
+    // set options or other chart code here
 var option;
 
 myChart1.showLoading();
@@ -170,13 +173,10 @@ option = {
 
 option && myChart.setOption(option);
 
+});
 
-// If you have dependencies that try to import CSS, esbuild will generate a separate `app.css` file.
-// To load it, simply add a second `<link>` to your `root.html.heex` file.
 
-// Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
 import "phoenix_html"
-// Establish Phoenix Socket and LiveView configuration.
 import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
 import {hooks as colocatedHooks} from "phoenix-colocated/tripwire"
@@ -194,31 +194,13 @@ topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
 window.addEventListener("phx:page-loading-start", _info => topbar.show(300))
 window.addEventListener("phx:page-loading-stop", _info => topbar.hide())
 
-// connect if there are any LiveViews on the page
 liveSocket.connect()
 
-// expose liveSocket on window for web console debug logs and latency simulation:
-// >> liveSocket.enableDebug()
-// >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
-// >> liveSocket.disableLatencySim()
 window.liveSocket = liveSocket
-
-// The lines below enable quality of life phoenix_live_reload
-// development features:
-//
-//     1. stream server logs to the browser console
-//     2. click on elements to jump to their definitions in your code editor
-//
 if (process.env.NODE_ENV === "development") {
   window.addEventListener("phx:live_reload:attached", ({detail: reloader}) => {
-    // Enable server log streaming to client.
-    // Disable with reloader.disableServerLogs()
     reloader.enableServerLogs()
 
-    // Open configured PLUG_EDITOR at file:line of the clicked element's HEEx component
-    //
-    //   * click with "c" key pressed to open at caller location
-    //   * click with "d" key pressed to open at function component definition location
     let keyDown
     window.addEventListener("keydown", e => keyDown = e.key)
     window.addEventListener("keyup", _e => keyDown = null)
--- web/tripwire_web/live/dashboard.ex
+++ web/tripwire_web/live/dashboard.ex
@@ -1,19 +1,19 @@
-defmodule TripwireWeb.SystemLive do
-  use TripwireWeb, :live_view
+# defmodule TripwireWeb.SystemLive do
+#   use TripwireWeb, :live_view
 
-  def render(assigns) do
-    ~H"""
-    <%!-- Current temperature: {@temperature}°F --%>
-    <%!-- <button phx-click="inc_temperature">+</button> --%>
-    """
-  end
+#   def render(assigns) do
+#     ~H"""
+#     <%!-- Current temperature: {@temperature}°F --%>
+#     <%!-- <button phx-click="inc_temperature">+</button> --%>
+#     """
+#   end
 
-  def mount(_params, _session, socket) do
-    # temperature = 70 # Let's assume a fixed temperature for now
-    # {:ok, assign(socket, :temperature, temperature)}
-  end
+#   def mount(_params, _session, socket) do
+#     # temperature = 70 # Let's assume a fixed temperature for now
+#     # {:ok, assign(socket, :temperature, temperature)}
+#   end
 
-  def handle_event("inc_temperature", _params, socket) do
-    # {:noreply, update(socket, :temperature, &(&1 + 1))}
-  end
-end
+#   def handle_event("inc_temperature", _params, socket) do
+#     # {:noreply, update(socket, :temperature, &(&1 + 1))}
+#   end
+# end
