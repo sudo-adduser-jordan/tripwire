642811f9826b46beecd97d453f1dd4813b931c60
jordan <sudo.sendmail.jordan@gmail.com>
2025-12-13 04:37:10 -0500

    Update settings_component.ex

--- web/components/settings_component.ex
+++ web/components/settings_component.ex
@@ -36,170 +36,245 @@ defmodule TripwireWeb.SettingsComponent do
 
   def settings(assigns) do
 ~H"""
-<section class="hidden size-full" id="settings-template" >
+<div class="hidden size-full" id="settings-template">
 
-<div class="tabs tabs-box size-full">
-  <input type="radio" name="my_tabs_2" class="tab" aria-label="Account" checked="checked" />
-  <div class="tab-content bg-base-100 border-base-300 p-6">
-    Account Settings
-    <section>
-    <div class="stats shadow">
-  <div class="stat">
-    <div class="stat-figure text-primary">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        class="inline-block h-8 w-8 stroke-current"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-        ></path>
-      </svg>
-    </div>
-    <div class="stat-title">Total Likes</div>
-    <div class="stat-value text-primary">25.6K</div>
-    <div class="stat-desc">21% more than last month</div>
-  </div>
+  <div class="tabs tabs-box size-full">
+    <input type="radio" name="my_tabs_6" class="tab" aria-label="Account" checked="checked" />
+    <div class="tab-content  bg-base-100 border-base-300 p-6">
 
-  <div class="stat">
-    <div class="stat-figure text-secondary">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        class="inline-block h-8 w-8 stroke-current"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M13 10V3L4 14h7v7l9-11h-7z"
-        ></path>
-      </svg>
-    </div>
-    <div class="stat-title">Page Views</div>
-    <div class="stat-value text-secondary">2.6M</div>
-    <div class="stat-desc">21% more than last month</div>
-  </div>
+    <div class="size-full flex flex-col gap-4">
+      Account
+      <div class="flex flex-row ">
+        <div class="stats shadow">
+          <div class="stat">
+            <div class="stat-figure text-secondary">
+              <div class="avatar avatar-online">
+                <div class="w-16 rounded-full">
+                  <img src="https://img.daisyui.com/images/profile/demo/anakeen@192.webp" />
+                </div>
+              </div>
 
-  <div class="stat">
-    <div class="stat-figure text-secondary">
-      <div class="avatar avatar-online">
-        <div class="w-16 rounded-full">
-          <img src="https://img.daisyui.com/images/profile/demo/anakeen@192.webp" />
+            </div>
+            <div class="stat-value">Characer Name1</div>
+            <div class="stat-title">Corporation Name</div>
+            <div class="stat-desc text-secondary">Masks</div>
+          </div>
         </div>
+        <div class="stats shadow">
+          <div class="stat">
+            <div class="stat-figure text-secondary">
+              <div class="avatar avatar-offline">
+                <div class="w-16 rounded-full">
+                  <img src="https://img.daisyui.com/images/profile/demo/anakeen@192.webp" />
+                </div>
+              </div>
+
+            </div>
+            <div class="stat-value">Characer Name2</div>
+            <div class="stat-title">Corporation Name</div>
+            <div class="stat-desc text-secondary">Masks</div>
+          </div>
+        </div>
+
+        <div class="stats shadow">
+          <div class="stat">
+            <div class="stat-figure text-secondary">
+              <div class="avatar avatar-offline">
+                <div class="w-16 rounded-full">
+                  <img src="https://img.daisyui.com/images/profile/demo/anakeen@192.webp" />
+                </div>
+              </div>
+
+            </div>
+            <div class="stat-value">Characer Name3</div>
+            <div class="stat-title">Corporation Name</div>
+            <div class="stat-desc text-secondary">Alliance Name</div>
+          </div>
+      </div>
+      </div>
+        <div class="flex flex-col gap-4">
+        Masks
+        <div class="badge badge-soft badge-primary ml-8 ">Mask 1</div>
+        <div class="badge badge-soft badge-secondary ml-8 ">Mask 2</div>
+        <div class="badge badge-soft badge-accent ml-8 ">Mask 3</div>
       </div>
     </div>
-    <div class="stat-value">86%</div>
-    <div class="stat-title">Tasks done</div>
-    <div class="stat-desc text-secondary">31 tasks remaining</div>
-  </div>
-</div>
-</section>
+    </div>
 
-  </div>
-  <input type="radio" name="my_tabs_2" class="tab" aria-label="Map" />
+
+
+
+
+  <input type="radio" name="my_tabs_6" class="tab" aria-label="Map" />
   <div class="tab-content bg-base-100 border-base-300 p-6">
     Map Settings
-    <section>
-        <input type="checkbox" checked="checked" class="toggle toggle-primary" />
-        <input type="checkbox" checked="checked" class="toggle toggle-secondary" />
-        <input type="checkbox" checked="checked" class="toggle toggle-accent" />
-        <input type="checkbox" checked="checked" class="toggle toggle-neutral" />
-        <input type="checkbox" checked="checked" class="toggle toggle-info" />
-        <input type="checkbox" checked="checked" class="toggle toggle-success" />
-        <input type="checkbox" checked="checked" class="toggle toggle-warning" />
-        <input type="checkbox" checked="checked" class="toggle toggle-error" />
-        <section>
-        <select class="select select-ghost">
-        <option disabled selected>Pick a font</option>
-        <option>Inter</option>
-        <option>Poppins</option>
-        <option>Raleway</option>
-        </select>
-    </section>
-        <section>
-        <select class="select select-ghost">
-        <option disabled selected>Pick a font</option>
-        <option>Inter</option>
-        <option>Poppins</option>
-        <option>Raleway</option>
-        </select>
-    </section>
-        <section>
-        <select class="select select-ghost">
-        <option disabled selected>Pick a font</option>
-        <option>Inter</option>
-        <option>Poppins</option>
-        <option>Raleway</option>
-        </select>
-    </section>
+    <div class="flex flex-col gap-4">
 
-    </section>
+
+
+      <div class="flex flex-row gap-4">
+        <p class="w-25/100 pl-8">System</p>
+        <select class="select select-ghost">
+          <option disabled selected>Pick a font</option>
+          <option>Inter</option>
+          <option>Poppins</option>
+          <option>Raleway</option>
+        </select>
+
+      </div>
+
+      <div class="flex flex-row gap-4">
+        <p class="w-25/100 pl-8">System</p>
+        <select class="select select-ghost">
+          <option disabled selected>Pick a font</option>
+          <option>Inter</option>
+          <option>Poppins</option>
+          <option>Raleway</option>
+        </select>
+      </div>
+
+      <div class="flex flex-row gap-4">
+        <p class="w-25/100 pl-8">System</p>
+        <select class="select select-ghost">
+          <option disabled selected>Pick a font</option>
+          <option>Inter</option>
+          <option>Poppins</option>
+          <option>Raleway</option>
+        </select>
+      </div>
+
+
+
+
+
+
+      <div class="flex flex-row gap-4">
+        <p class="w-4/10 pl-8">System</p>
+        <input type="checkbox" checked="checked" class="toggle toggle-primary" />
+      </div>
+      <div class="flex flex-row gap-4">
+        <p class="w-4/10 pl-8">System</p>
+        <input type="checkbox" checked="checked" class="toggle toggle-secondary" />
+      </div>
+      <div class="flex flex-row gap-4">
+        <p class="w-4/10 pl-8">System</p>
+        <input type="checkbox" checked="checked" class="toggle toggle-accent" />
+      </div>
+      <div class="flex flex-row gap-4">
+        <p class="w-4/10 pl-8">System</p>
+        <input type="checkbox" checked="checked" class="toggle toggle-neutral" />
+      </div>
+      <div class="flex flex-row gap-4">
+        <p class="w-4/10 pl-8">System</p>
+        <input type="checkbox" checked="checked" class="toggle toggle-info" />
+      </div>
+      <div class="flex flex-row gap-4">
+        <p class="w-4/10 pl-8">System</p>
+        <input type="checkbox" checked="checked" class="toggle toggle-success" />
+      </div>
+      <div class="flex flex-row gap-4">
+        <p class="w-4/10 pl-8">System</p>
+        <input type="checkbox" checked="checked" class="toggle toggle-warning" />
+      </div>
+      <div class="flex flex-row gap-4">
+        <p class="w-4/10 pl-8">System</p>
+        <input type="checkbox" checked="checked" class="toggle toggle-error" />
+      </div>
+
+    </div>
   </div>
 
-  <input type="radio" name="my_tabs_2" class="tab" aria-label="Widgets" />
+  <input type="radio" name="my_tabs_6" class="tab" aria-label="Widgets" />
   <div class="tab-content bg-base-100 border-base-300 p-6">
     Widgets
-    <section >
-    <p>System</p>
-    <input type="checkbox" checked="checked" class="checkbox checkbox-primary" />
+    <div>
 
-    <p>Notes</p>
-    <input type="checkbox" checked="checked" class="checkbox checkbox-secondary" />
+      <div class="flex flex-row gap-4">
+        <input type="checkbox" checked="checked" class="checkbox checkbox-primary" />
+        <p>System</p>
+      </div>
 
-    <p>Signatures</p>
-    <input type="checkbox" checked="checked" class="checkbox checkbox-accent" />
+      <div class="flex flex-row gap-4">
+        <input type="checkbox" checked="checked" class="checkbox checkbox-secondary" />
+        <p>Notes</p>
+      </div>
 
-    <p>Map</p>
-    <input type="checkbox" checked="checked" class="checkbox checkbox-neutral" />
+      <div class="flex flex-row gap-4">
+        <input type="checkbox" checked="checked" class="checkbox checkbox-accent" />
+        <p>Signatures</p>
+      </div>
 
-    <p>Killboard</p>
-        <input type="checkbox" checked="checked" class="checkbox checkbox-error" />
+      <div class="flex flex-row gap-4">
+        <input type="checkbox" checked="checked" class="checkbox checkbox-neutral" />
+        <p>Map</p>
+      </div>
 
-    <p>Player Statistics</p>
-        <input type="checkbox" checked="checked" class="checkbox checkbox-info" />
+      <div class="flex flex-row gap-4">
+        <input type="checkbox" class="checkbox checkbox-error" />
+        <p>Killboard</p>
+      </div>
 
-    <p>Discord Intell Channel</p>
-        <input type="checkbox" checked="checked" class="checkbox checkbox-success" />
+      <div class="flex flex-row gap-4">
+        <input type="checkbox" class="checkbox checkbox-info" />
+        <p>Player Statistics</p>
+      </div>
 
-    <p>Udema Scout</p>
-        <input type="checkbox" checked="checked" class="checkbox checkbox-warning" />
-    </section>
+      <div class="flex flex-row gap-4">
+        <input type="checkbox" class="checkbox checkbox-success" />
+        <p>Discord Intell</p>
+      </div>
+
+      <div class="flex flex-row gap-4">
+        <input type="checkbox" class="checkbox checkbox-warning" />
+        <p>Udema Scout</p>
+      </div>
+
+    </div>
   </div>
 
-  <input type="radio" name="my_tabs_2" class="tab" aria-label="Theme" />
+  <input type="radio" name="my_tabs_6" class="tab" aria-label="Theme" />
   <div class="tab-content bg-base-100 border-base-300 p-6">
     Themes
-    <section >
-    <p>Default</p>
-    <input type="checkbox" checked="checked" class="checkbox checkbox-primary" />
+    <div>
 
-    <p>Native</p>
-    <input type="checkbox" class="checkbox checkbox-secondary" />
+      <div class="flex flex-row gap-4">
+        <input type="checkbox" checked="checked" class="checkbox checkbox-primary" />
+        <p>Default</p>
+      </div>
 
-    <p>Caldari</p>
-    <input type="checkbox" class="checkbox checkbox-info" />
+      <div class="flex flex-row gap-4">
+        <input type="checkbox" class="checkbox checkbox-secondary" />
+        <p>Native</p>
+      </div>
 
-    <p>Gallente</p>
-    <input type="checkbox" class="checkbox checkbox-success" />
-
-    <p>Minmatar</p>
-        <input type="checkbox" class="checkbox checkbox-error" />
-
-    <p>Amarr</p>
+      <div class="flex flex-row gap-4">
         <input type="checkbox" class="checkbox checkbox-info" />
+        <p>Caldari</p>
+      </div>
 
-    </section>
+      <div class="flex flex-row gap-4">
+        <input type="checkbox" class="checkbox checkbox-success" />
+        <p>Gallente</p>
+      </div>
+
+      <div class="flex flex-row gap-4">
+        <input type="checkbox" class="checkbox checkbox-error" />
+        <p>Minmatar</p>
+      </div>
+
+      <div class="flex flex-row gap-4">
+        <input type="checkbox" class="checkbox checkbox-info" />
+        <p>Amarr</p>
+      </div>
+
+    </div>
   </div>
+
+
+  </div>
+
+
 </div>
-
-
-</section>
 """
     end
 end
